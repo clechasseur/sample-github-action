beddb637323202b3678e9ec68709e0108d0b3d4b
Charles Lechasseur <shiftingbeard@gmx.com>
2021-04-14 12:30:57 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,4 @@
 echo "Running the action"
 echo "Here is the payload of the event:"
 cat $GITHUB_EVENT_PATH
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World! - from clechasseur"}' $1
