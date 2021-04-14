afd77adaf7ab2d688d4ede7036d1c70ff5e253b9
Charles Lechasseur <shiftingbeard@gmx.com>
2021-04-14 12:39:54 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 echo "Running the action"
 echo "Here is the payload of the event:"
 cat $GITHUB_EVENT_PATH
-output_send_message=$(curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' $1)
+output_send_message=$(curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World! from clechasseur"}' $1)
 if [[ "$output_send_message" == "ok" ]]; then
   echo "Notification sent properly"
   output=0
